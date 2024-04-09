7801db96c26eabf8897e7e6bdeca7d51ba19c552
Bernardo Castaldelli <166317065+becastal@users.noreply.github.com>
2024-04-09 12:18:29 -0300

    Update #chegandoJunto.cpp

--- fei/07grafosI/#chegandoJunto.cpp
+++ fei/07grafosI/#chegandoJunto.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <set>
+#include <map>
 #define f first
 #define s second
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
@@ -15,60 +16,99 @@ using namespace std;
 const vector<pair<int, int>> movimentos = { {0, -1}, {0, 1}, {1, 0}, {-1, 0} };
 vector<vector<char>> mapa;
 vector<pair<int, int>> robos;
-vector<pair<int, int>> destinos;
-vector<vector<vector<int, int>>> distancia;
-set<pair<int, int>> destinos;
-vector<vector<vector<int>>> distancia;
+map<vector<pair<int, int>>, int> distancia;
 int n;
 
-void bfs()
-bool movimentoValido(pair<int, int> mov)
+bool movimentosValidos(vector<pair<int, int>> posicoes, pair<int, int> movimento)
 {
-    bool valido = true;
-
+    bool resposta = false;
+    vector<pair<int, int>> novasPosicoes;
+    for (int i = 0; i < 3; i++)
+    {
+        pair<int, int> pos = posicoes[i];
+        pair<int, int> novaPos = { pos.f + movimento.f, pos.s + movimento.s };
+        if (novaPos.f >= 0 && novaPos.f < n && novaPos.s >= 0 && novaPos.s < n && mapa[novaPos.f][novaPos.s] != '#') resposta = true;
+    }
+    if (resposta) if (distancia[novasPosicoes] != 0) resposta = false;
+    return(resposta);
 }
 
-bool chegaramNosDestinos()
+bool chegaramJunto(vector<pair<int, int>> posicoes)
 {
+    for (auto pos : posicoes)
+    {
+        if (mapa[pos.f][pos.s] != 'X') return(false);
+    }
+    return(true);
+}
 
+bool vetorUnico(vector<pair<int, int>> vetor)
+{
+    set<pair<int, int>> s;
+    for (auto v : vetor) s.insert(v);
+    return(vetor.size() == s.size());
+}
+
+vector<pair<int, int>> atualizaPosicoes(vector<pair<int, int>> posicoes, pair<int, int> movimento)
+{
+    vector<pair<int, int>> resposta;
+    for (int i = 0; i < 3; i++)
+    {
+        pair<int, int> pos = posicoes[i];
+        pair<int, int> novaPos = { pos.f + movimento.f, pos.s + movimento.s };
+        if (novaPos.f >= 0 && novaPos.f < n && novaPos.s >= 0 && novaPos.s < n && mapa[novaPos.f][novaPos.s] != '#')
+        {
+            resposta.push_back(novaPos); 
+        }
+        else
+        {
+            resposta.push_back(pos); 
+        }
+    }
+
+    return(resposta);
 }
 
 int bfs()
 {
-    queue<pair<int, int>> q;
-
-    q.push({ 0, 0 });
-    
+    distancia.clear();
+    queue<vector<pair<int, int>>> q;
+    q.push(robos);
+    distancia[robos] = 0;
 
     while (!q.empty())
     {
-        pair<int, int> v = q.front(); q.pop();
-        for (auto u : movimentos)
+        vector<pair<int, int>> posicoesAtuais = q.front(); q.pop();
+        if (chegaramJunto(posicoesAtuais)) return(distancia[posicoesAtuais]);
+        for (pair<int, int> mov : movimentos)
         {
-            if (movimentoValido(u))
+            if (movimentosValidos(posicoesAtuais, mov))
             {
-                // sei se eh tao simples!
+                vector<pair<int, int>> posicoesNovas = atualizaPosicoes(posicoesAtuais, mov);
+                if (vetorUnico(posicoesNovas))
+                {
+                    q.push(posicoesNovas);
+                    distancia[posicoesNovas] = distancia[posicoesAtuais] + 1;
+                }
             }
         }
     }
+    return (-1);
 }
 
 int main()
 {
-    int t; cin >> t;
-    while (t--)
-        int T; cin >> T;
+    int T; cin >> T;
     for (int t = 1; t <= T; t++)
     {
-        int n; cin >> n;
+        cin >> n;
+        mapa = vector<vector<char>>(n, vector<char>(n));
         robos = vector<pair<int, int>>(3);
         for (int i = 0; i < n; i++) for (int j = 0; j < n; j++)
         {
             cin >> mapa[i][j];
-            if (mapa[i][j] >= 'A' && mapa[i][j] <= 'C') robos.push_back({ i, j });
-            if (mapa[i][j] == '#') destinos.push_back({ i, j });
+            if (mapa[i][j] >= 'A' && mapa[i][j] <= 'C') robos[mapa[i][j] - 'A'] =  { i, j };
         }
-
 
         int resposta = bfs();
         cout << "Case " << t << ": ";

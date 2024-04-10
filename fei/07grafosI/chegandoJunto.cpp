6018f79bc5e36f3c34b658dbf752914a33be7a55
Bernardo Castaldelli <166317065+becastal@users.noreply.github.com>
2024-04-10 10:54:20 -0300

    Update and rename #chegandoJunto.cpp to chegandoJunto.cpp

--- fei/07grafosI/chegandoJunto.cpp
+++ fei/07grafosI/chegandoJunto.cpp
@@ -13,7 +13,7 @@ const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 using namespace std;
 
-const vector<pair<int, int>> movimentos = { {0, -1}, {0, 1}, {1, 0}, {-1, 0} };
+const vector<pair<int, int>> movimentos = { {-1, 0}, {1, 0}, {0, 1}, {0, -1} };
 vector<vector<char>> mapa;
 vector<pair<int, int>> robos;
 map<vector<pair<int, int>>, int> distancia;
@@ -21,16 +21,13 @@ int n;
 
 bool movimentosValidos(vector<pair<int, int>> posicoes, pair<int, int> movimento)
 {
-    bool resposta = false;
-    vector<pair<int, int>> novasPosicoes;
     for (int i = 0; i < 3; i++)
     {
         pair<int, int> pos = posicoes[i];
         pair<int, int> novaPos = { pos.f + movimento.f, pos.s + movimento.s };
-        if (novaPos.f >= 0 && novaPos.f < n && novaPos.s >= 0 && novaPos.s < n && mapa[novaPos.f][novaPos.s] != '#') resposta = true;
+        if (novaPos.f >= 0 && novaPos.f < n && novaPos.s >= 0 && novaPos.s < n && mapa[novaPos.f][novaPos.s] != '#') return(true);
     }
-    if (resposta) if (distancia.find(novasPosicoes) != distancia.end()) resposta = false;
-    return(resposta);
+    return(false);
 }
 
 bool chegaramJunto(vector<pair<int, int>> posicoes)
@@ -51,22 +48,44 @@ bool vetorUnico(vector<pair<int, int>> vetor)
 
 vector<pair<int, int>> atualizaPosicoes(vector<pair<int, int>> posicoes, pair<int, int> movimento)
 {
-    vector<pair<int, int>> resposta;
+    vector<pair<int, int>> resposta (3);
+    vector<bool> movimentar(3, false);
+    map<pair<int, int>, int> contadorPosicoes;
     for (int i = 0; i < 3; i++)
     {
         pair<int, int> pos = posicoes[i];
         pair<int, int> novaPos = { pos.f + movimento.f, pos.s + movimento.s };
-        if (novaPos.f >= 0 && novaPos.f < n && novaPos.s >= 0 && novaPos.s < n && mapa[novaPos.f][novaPos.s] != '#')
-        {
-            resposta.push_back(novaPos); 
-        }
-        else
-        {
-            resposta.push_back(pos); 
-        }
+        movimentar[i] = (novaPos.f >= 0 && novaPos.f < n && novaPos.s >= 0 && novaPos.s < n && mapa[novaPos.f][novaPos.s] != '#');
+        if (movimentar[i]) resposta[i] = novaPos; else resposta[i] = pos;
+        contadorPosicoes[resposta[i]]++;
     }
 
+    for (int i = 0; i < 3; i++)
+        if (movimentar[i] && contadorPosicoes[resposta[i]] > 1) 
+            resposta[i].f -= movimento.f, resposta[i].s -= movimento.s;
+
     return(resposta);
+}
+
+void printMapaAtual(vector<pair<int, int>> posicoes)
+{
+    auto mapaAtual = mapa;
+    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) if (mapa[i][j] >= 'A' && mapa[i][j] <= 'C') mapaAtual[i][j] = '.'; else mapaAtual[i][j] = mapa[i][j];
+
+    cout << endl << "posicao alcancada em [" << distancia[posicoes] << "] movimentos!";
+    for (int i = 0; i < 3; i++)
+    {
+        cout << char(i + 'A') << "{" << posicoes[i].f << ", " << posicoes[i].s << "} ";
+        mapaAtual[posicoes[i].f][posicoes[i].s] = i + 'A';
+    }
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++) cout << mapaAtual[i][j];
+        cout << endl;
+    }
+
 }
 
 int bfs()
@@ -79,13 +98,14 @@ int bfs()
     while (!q.empty())
     {
         vector<pair<int, int>> posicoesAtuais = q.front(); q.pop();
+        // printMapaAtual(posicoesAtuais);
         if (chegaramJunto(posicoesAtuais)) return(distancia[posicoesAtuais]);
         for (pair<int, int> mov : movimentos)
         {
             if (movimentosValidos(posicoesAtuais, mov))
             {
                 vector<pair<int, int>> posicoesNovas = atualizaPosicoes(posicoesAtuais, mov);
-                if (vetorUnico(posicoesNovas))
+                if (vetorUnico(posicoesNovas) && distancia.find(posicoesNovas) == distancia.end())
                 {
                     q.push(posicoesNovas);
                     distancia[posicoesNovas] = distancia[posicoesAtuais] + 1;
@@ -96,7 +116,6 @@ int bfs()
     return (-1);
 }
 
-// algum problema com linhas e colunas.
 int main()
 {
     int T; cin >> T;
@@ -108,7 +127,7 @@ int main()
         for (int i = 0; i < n; i++) for (int j = 0; j < n; j++)
         {
             cin >> mapa[i][j];
-            if (mapa[i][j] >= 'A' && mapa[i][j] <= 'C') robos[mapa[i][j] - 'A'] =  { i, j };
+            if (mapa[i][j] >= 'A' && mapa[i][j] <= 'C') robos[mapa[i][j] - 'A'] = { i, j };
         }
 
         int resposta = bfs();

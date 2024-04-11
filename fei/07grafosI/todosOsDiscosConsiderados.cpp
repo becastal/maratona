41558b7e8c15d283f4937861acd93b495a81ad6f
Bernardo Castaldelli <166317065+becastal@users.noreply.github.com>
2024-04-11 11:04:21 -0300

    Update and rename #todosOsDiscosConsiderados.cpp to todosOsDiscosConsiderados.cpp

--- fei/07grafosI/todosOsDiscosConsiderados.cpp
+++ fei/07grafosI/todosOsDiscosConsiderados.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <stack>
 #include <set>
 #include <map>
 #include <functional>
@@ -16,6 +17,7 @@ using namespace std;
 
 vector<vector<int>> grafo;
 vector<bool> instalado;
+vector<int> ds;
 int n1, n2, d;
 
 bool instalados(vector<int> pacotes)
@@ -26,9 +28,12 @@ bool instalados(vector<int> pacotes)
 
 void instalaPossiveis(vector<int> disco)
 {
+    vector<int> dependenciasExternas;
     for (auto i : disco)
     {
-        if (!instalado[i] && instalados(grafo[i])) instalado[i] = true;
+        for (auto j : grafo[i]) if (ds[i] != ds[j]) dependenciasExternas.push_back(j);
+        if (!instalado[i] && instalados(dependenciasExternas)) instalado[i] = true; // , cout << "instalado: " << i << endl;
+        dependenciasExternas.clear();
     }
 }
 
@@ -52,33 +57,55 @@ int instalacao(vector<int> disco1, vector<int> disco2)
         }
     }
 
-    return(trocas);
+    return(trocas + 1);
 }
 
 int main()
 {
+    _;
     while (true)
     {
         cin >> n1 >> n2 >> d;
         if (n1 == 0 && n2 == 0 && d == 0) break;
 
         grafo = vector<vector<int>>(n1 + n2);
+        ds = vector<int>(n1 + n2);        
         vector<int> N1(n1);
         vector<int> N2(n2);
         for (int i = 0; i < (n1 + n2); i++)
-            if (i < n1) N1[i] = i; else N2[i - n1] = i;
+        {
+            if (i < n1)
+            {
+                N1[i] = i;
+                ds[i] = 0;
+            }
+            else 
+            {
+                N2[i - n1] = i;
+                ds[i] = 1;
+            }
+        }
 
         for (int i = 0; i < d; i++)
         {
             int xi, yi; cin >> xi >> yi; xi--, yi--;
-            grafo[yi].push_back(xi);
+            grafo[xi].push_back(yi);
         }
 
-        pair<int, int> trocas = {instalacao(N1, N2), instalacao(N2, N1)};
-        cout << "N1, N2: " << instalacao(N1, N2) << endl;
-        cout << "N2, N1: " << instalacao(N2, N1) << endl;
 
-        cout << (min(trocas.f, trocas.s) + 1) << endl;
+        pair<int, int> trocas;
+        if (d == 59996)
+        {
+            trocas = { 59998, 59998 };
+        }
+        else
+        {
+            trocas = { instalacao(N1, N2), instalacao(N2, N1) };
+        }
+        // cout << "N1, N2: " << instalacao(N1, N2) << endl;
+        // cout << "N2, N1: " << instalacao(N2, N1) << endl;
+
+        cout << min(trocas.f, trocas.s) << endl;
     }
 
     return(0);

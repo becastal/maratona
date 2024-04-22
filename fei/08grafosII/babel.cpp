21b096753b983cca2731c17053151c1140e4829b
Bernardo Castaldelli <166317065+becastal@users.noreply.github.com>
2024-04-22 14:09:06 -0300

    Create babel.cpp

---
+++ fei/08grafosII/babel.cpp
@@ -0,0 +1,4 @@
+// entrar dados como lista de adjascencia map<string, vector<string>>
+// bfs que calcula a distancia de uma palavra pra todas as outras mas so quando bate os criterios que o enunciado fala
+// guardar valores das dists em um grafo ponderado sendo as distancias os pesos das arestas
+// kruskal nesse novo grafo pra gerar maximum spaning tree

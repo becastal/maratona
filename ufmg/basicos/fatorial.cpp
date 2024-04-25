0e97cfd69ad13321abb29f157594f7baed2a1bc3
Bernardo Castaldelli <166317065+becastal@users.noreply.github.com>
2024-04-25 11:12:12 -0300

    Update and rename 1_fatorial.cpp to fatorial.cpp

--- ufmg/basicos/fatorial.cpp
+++ ufmg/basicos/fatorial.cpp
@@ -1,18 +1,25 @@
 // https://neps.academy/br/course/introducao-a-programacao/lesson/fatorial
 
 #include <bits/stdc++.h>
+#define f first
+#define s second
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
+#define dbg(x) cout << #x << " = " << x << endl
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 using namespace std;
 
 int main()
-{ _
+{
+    _;
 	int n; cin >> n;
     int resultado = 1;
     
     for (int i = 2; i <= n; i++)
-    {
         resultado *= i;
-    }
+    
     cout << resultado << endl;
+    return(0);
 }

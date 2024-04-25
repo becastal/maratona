9aa131cfd2d07f1219ea5fde07e3b1bc802aacc4
Bernardo Castaldelli <166317065+becastal@users.noreply.github.com>
2024-04-25 11:20:39 -0300

    Update and rename 1_primo.cpp to primo.cpp

--- ufmg/basicos/primo.cpp
+++ ufmg/basicos/primo.cpp
@@ -1,22 +1,28 @@
 // https://br.spoj.com/problems/PRIMO/
 
 #include <bits/stdc++.h>
+#define f first
+#define s second
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
+#define dbg(x) cout << #x << " = " << x << endl
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
 using namespace std;
 
 int main()
-{ _
+{
+    _;
 	int n; cin >> n;
 
-    string resposta = "sim";
+    bool resposta = true;
     for (int i = 2; i*i <= n; i++)
-    {
         if (n % i == 0) 
         {
-            resposta = "nao";
+            resposta = false;
             break;
         }
-    }
-    cout << resposta << endl;
+
+    cout << (resposta ? "sim" : "nao") << endl;
 }
